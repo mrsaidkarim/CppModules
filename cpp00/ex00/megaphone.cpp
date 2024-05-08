a00c99352285785b2b4b967dff4749c044164e40
SaidKarim-web <sirsaidkarim@gmail.com>
2024-05-08 21:05:31 +0100

    Modify megaphone src file

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 17:28:44 by skarim            #+#    #+#             */
-/*   Updated: 2024/05/04 18:03:34 by skarim           ###   ########.fr       */
+/*   Updated: 2024/05/08 21:03:31 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,19 @@
 int main(int ac, char **av)
 {
 	if (ac == 1)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
 	else
 	{
 		for (int i = 1; i < ac; i++)
 		{
-			for(int j = 0; av[i][j]; j++)
-				av[i][j] = toupper(av[i][j]);
-			std::cout << av[i];
+			std::string input = av[i];
+			for (size_t j = 0; j < input.size(); j++)
+			{
+				input[j] = toupper(input[j]);
+			}
+			std::cout << input;
 		}
 		std::cout << std::endl;
 	}
