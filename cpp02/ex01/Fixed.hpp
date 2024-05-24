857cd70581a51c0e712882617c2b072186f91ea5
SaidKarim-web <sirsaidkarim@gmail.com>
2024-05-24 20:41:07 +0100

    Add the header file

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 12:35:05 by skarim            #+#    #+#             */
+/*   Updated: 2024/05/23 18:06:07 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	private:
+		int					fixedPointValue;
+		static const int	fractionalBits = 8;
+	public:
+		Fixed();
+		Fixed(const int nbr);
+		Fixed(const float nbr);
+		Fixed(const Fixed &f);
+		~Fixed();
+		
+		Fixed &operator=(const Fixed &f);
+		
+		int getRawBits( void ) const;
+		void setRawBits( int const fp );
+
+		float toFloat( void ) const;
+		int toInt( void ) const;
+};
+
+std::ostream& operator<<(std::ostream &out, const Fixed &f);  
+
+#endif
