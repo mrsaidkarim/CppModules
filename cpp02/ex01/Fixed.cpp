3864b3dff6f31c78cd0ba0fad28dd24fc66f22da
SaidKarim-web <sirsaidkarim@gmail.com>
2024-05-24 20:40:56 +0100

    Add source files

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 12:35:39 by skarim            #+#    #+#             */
+/*   Updated: 2024/05/24 11:05:29 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &f)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = f;
+}
+
+Fixed::Fixed(const int nbr)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->fixedPointValue = nbr << this->fractionalBits;
+}
+
+Fixed::Fixed(const float nbr)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->fixedPointValue = roundf(nbr * (1 << this->fractionalBits));
+}
+
+Fixed &Fixed::operator=(const Fixed &f)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &f)
+	{
+		this->fixedPointValue = f.fixedPointValue;
+	}
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	return (this->fixedPointValue);
+}
+
+void Fixed::setRawBits( int const fp )
+{
+	this->fixedPointValue = fp;
+}
+
+float Fixed::toFloat( void ) const
+{
+	return ((float)this->fixedPointValue / (1 << this->fractionalBits));
+}
+
+int Fixed::toInt( void ) const
+{
+	return (this->fixedPointValue >> this->fractionalBits);
+}
+
+std::ostream& operator<<(std::ostream &out, const Fixed &f)
+{
+	out << f.toFloat();
+	return (out);
+}
---
+++ cpp02/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 12:35:53 by skarim            #+#    #+#             */
+/*   Updated: 2024/05/24 11:01:50 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return 0;
+}
