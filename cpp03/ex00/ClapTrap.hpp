9ef0c756c89f31a3efb620d930eec6f9a1c3cea0
SaidKarim-web <sirsaidkarim@gmail.com>
2024-06-01 21:28:53 +0100

    Modify the header file

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:40:39 by skarim            #+#    #+#             */
-/*   Updated: 2024/05/30 16:33:05 by skarim           ###   ########.fr       */
+/*   Updated: 2024/06/01 09:31:49 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ class ClapTrap
 {
 	private:
 		std::string name;
-		unsigned int	hitPoints;
-		unsigned int	energyPoints;
-		unsigned int	attackDamage;
+		int	hitPoints;
+		int	energyPoints;
+		int	attackDamage;
 	public:
 		ClapTrap();
 		ClapTrap(const std::string &name);
