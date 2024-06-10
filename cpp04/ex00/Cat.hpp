2199b643c1b050df346edc842ffbec5c46aa8fd5
SaidKarim-web <sirsaidkarim@gmail.com>
2024-06-10 20:42:20 +0100

    Add the header files

---
+++ cpp04/ex00/Cat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 10:17:24 by skarim            #+#    #+#             */
+/*   Updated: 2024/06/05 22:10:57 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+	public:
+		Cat();
+		Cat(const Cat &);
+		~Cat();
+		
+		Cat &operator=(const Cat &);
+		void makeSound() const;
+};
+
+#endif
---
+++ cpp04/ex00/Dog.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 10:17:36 by skarim            #+#    #+#             */
+/*   Updated: 2024/06/05 22:10:52 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+	public:
+		Dog();
+		Dog(const Dog &);
+		~Dog();
+		
+		Dog &operator=(const Dog &);
+		void makeSound() const;
+};
+
+#endif
